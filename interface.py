31036192fab1a7bd101eb75adf02d3f45b0f8e8d
vtnguyen04 <64852371+vtnguyen04@users.noreply.github.com>
2024-06-10 00:24:36 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -14,20 +14,20 @@ class StartupApp(QWidget):
         super().__init__()
         self.current_text_index = 0
         self.text_lines = [
-            "Major: Robotics and artifficial intelligence",
-            "Project Title: PCB FAULT DETECTION",
-            "Instructor: Ts. Nguyễn Văn Thái",
-            "Võ Thành Nguyễn, Student ID: 22134008",
-            "Phạm Văn Thịnh, Student ID: 22134014", 
-            "Phạm Ngọc Phúc, Student ID: 22134010"
+            "Major - Robotics and Artifficial Intelligence",
+            "Project Title - PCB FAULT DETECTION",
+            "Instructor - Ts. Nguyễn Văn Thái",
+            "Võ Thành Nguyễn - Student ID: 22134008",
+            "Phạm Văn Thịnh - Student ID: 22134014", 
+            "Phạm Ngọc Phúc - Student ID: 22134010"
         ]
         self.background_images = [
-            "robot_hand_background.png",  # Hình nền cho Major
-            "testresult.png",  # Hình nền cho Project Title
-            "thay.png",         # Hình nền cho Dr. Nguyễn Văn Thái
-            "nguyen.png",
-            "thinh.png",
-            "phuc.png"   
+            "images/robot_hand_background.png",  # Hình nền cho Major
+            "images/testresult.png",  # Hình nền cho Project Title
+            "images/thay.png",         # Hình nền cho Dr. Nguyễn Văn Thái
+            "images/member1.png",
+            "images/member2.png",
+            "images/member3.png"   
         ]
         self.App = App()
         self.initUI()
@@ -35,42 +35,36 @@ class StartupApp(QWidget):
 
     def initUI(self):
         self.setWindowTitle("Welcome to PCB FAULT DETECTION App")
-        self.setGeometry(100, 100, 700, 700)
-        self.setStyleSheet("background-color: #E0E6ED;")
+        self.setGeometry(100, 0, 1000, 1000)
+        self.setStyleSheet("background-color: #e0f7fa;")
 
         self.layout = QVBoxLayout()
 
         self.setLayout(self.layout)
-	
+            
+        logos_layout = QHBoxLayout()
+        school_logo_label = QLabel()
+        school_logo_pixmap = QPixmap("images/logo.png")
+        school_logo_label.setPixmap(school_logo_pixmap.scaled(500, 150, Qt.AspectRatioMode.KeepAspectRatio))
+        department_logo_label = QLabel()
+        department_logo_pixmap = QPixmap("images/khoa.png")
+        department_logo_label.setPixmap(department_logo_pixmap.scaled(450, 150, Qt.AspectRatioMode.KeepAspectRatio))
+        logos_layout.addWidget(school_logo_label, alignment=Qt.AlignmentFlag.AlignLeft)
+        logos_layout.addWidget(department_logo_label, alignment=Qt.AlignmentFlag.AlignRight)
+        self.layout.addLayout(logos_layout)
+        
         # Text Label
         self.text_label = QLabel("", self)
-        self.text_label.setStyleSheet("color: #4D6086; font-size: 25px;")
+        self.text_label.setStyleSheet("color: #4D6086; font-size: 23px;")
         self.text_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.text_label)
         
         self.background_label = QLabel(self)
         self.background_label.setScaledContents(True)
-        self.background_label.setFixedSize(600, 700)
+        self.background_label.setFixedSize(1000, 700)
 
         self.layout.addWidget(self.background_label)
-        
-        self.logo_label = QLabel(self)
-        pixmap_logo = QPixmap("logo.png")
-        self.logo_label.setPixmap(pixmap_logo)
-        self.logo_label.setScaledContents(True)
-        self.logo_label.resize(190, 150)  # Adjust size as needed
-        self.logo_label.move(50, 80)
 	    
-        
-        self.university_label = QLabel("Ho Chi Minh City University of \n  Technology and Education", self.background_label)
-        self.university_label.setStyleSheet("""
-					    color: #4D6086; 
-					    font-size: 15px; 
-					    font-weight: bold; 
-					""")
-        self.university_label.move(20, 190)  # Điều chỉnh vị trí phù hợp
-        
-        
         
         # Start Button
         self.startButton = QPushButton("Start App", self)
@@ -86,13 +80,11 @@ class StartupApp(QWidget):
     def updateText(self):
         if self.current_text_index < len(self.text_lines):
             self.text_label.setText(self.text_lines[self.current_text_index])
-            self.university_label.setText("Ho Chi Minh City University of \n  Technology and Education") 
             pixmap = QPixmap(self.background_images[self.current_text_index])	
             self.background_label.setPixmap(pixmap)
             self.current_text_index += 1
         else:
             self.text_label.setText("")  # Optionally clear/reset text after the last line
-            self.university_label.setText("")
             self.background_label.setPixmap(QPixmap())  # Clear the background image
             self.current_text_index = 0  # Reset index to loop the text
 
